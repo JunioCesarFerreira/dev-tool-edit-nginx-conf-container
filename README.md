e7e432ff2ee2415576df23ef8d2537dd169347bf
Junio Cesar Ferreira <jcf_ssp@hotmail.com>
2024-01-23 23:08:36 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,44 @@
+# Dev Tool Edit Nginx Configuration in Docker Container
+
+## Descrição
+Este projeto é uma ferramenta de desenvolvimento para editar arquivos de configuração do Nginx em containers Docker. Permite aos usuários especificar o nome do container e editar o arquivo `nginx.conf` diretamente através de uma interface web.
+
+## Instalação
+Siga estas etapas para instalar o projeto:
+
+## Pré-requisitos
+
+Certifique-se de ter o Python e o Docker instalados no seu sistema.
+
+```bash
+pip install Flask
+```
+
+## Como Executar
+
+1. Instale as dependências:
+
+```bash
+pip install -r requirements.txt
+```
+
+2. Execute o aplicativo Flask:
+
+```bash
+python app.py
+```
+
+O aplicativo estará disponível em http://localhost:3000/.
+
+## Uso
+
+1. Acesse http://localhost:3000/ no seu navegador.
+2. Clique em "Editar".
+3. Insira o caminho do arquivo (por exemplo, `/etc/nginx/nginx.conf`).
+4. Modifique o conteúdo do arquivo conforme necessário.
+5. Clique em "Registrar" para salvar as alterações no arquivo e reiniciar o container.
+
+## Notas Importantes
+
+- Este projeto é destinado apenas para fins de desenvolvimento local.
+- Modificar diretamente arquivos em um container em execução em ambientes de produção é uma prática arriscada.
---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+import subprocess
+import os
+
+app = Flask(__name__)
+
+nginx_conf_path = '/etc/nginx/nginx.conf'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/edit', methods=['POST'])
+def edit():
+    container_name = request.form['container_name']
+    
+    # Recupera o conteúdo atual do arquivo no container
+    result = subprocess.run(['docker', 'exec', container_name, 'cat', nginx_conf_path], capture_output=True, text=True)
+    current_content = result.stdout
+
+    return render_template('edit.html', file_path=nginx_conf_path, current_content=current_content)
+
+@app.route('/save', methods=['POST'])
+def save():
+    container_name = request.form['container_name']
+    content = request.form['content']
+
+    # Registra o conteúdo no arquivo no sistema de arquivos local
+    with open('nginx.conf', 'w') as f:
+        f.write(content)
+
+    # Copia o arquivo para o container
+    subprocess.run(['docker', 'cp', 'nginx.conf', f'{container_name}:{nginx_conf_path}'])
+
+    # Reinicia o container
+    subprocess.run(['docker', 'restart', container_name])
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+    
+    # Apaga o arquivo temporário após a execução do Flask
+    temp_file_path = 'nginx.conf'
+    if os.path.exists(temp_file_path):
+        os.remove(temp_file_path)
